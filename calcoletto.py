659a5c4ac414a36422d83cc4e33ed731253c581a
Simopane59 <149668828+Simopane59@users.noreply.github.com>
2024-02-26 22:23:39 +0100

    Create calcoletto.py

---
+++ calcoletto.py
@@ -0,0 +1,18 @@
+import threading
+
+def calcola(operazione):
+    global risultato
+    ris=operazione
+    risultato*=ris
+#((3 + 4) * (5 + 2) * (3 * 3)) / 2
+if __name__ == "__main__":
+    risultato=1
+    threads=[]
+    operazioni=[(3 + 4) , (5 + 2) , (3 * 3)]
+    for operazione in operazioni:
+        t = threading.Thread(target=calcola(operazione))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+    print("il risultato è ", risultato/2)
